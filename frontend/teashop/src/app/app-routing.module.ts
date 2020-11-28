4d9a38a1daa8996734f5f8f8e9c0f87e0e3cc9b1
DK <kinastowskid@gmail.com>
2020-11-28 18:07:06 +0100

    front

--- frontend/teashop/src/app/app-routing.module.ts
+++ frontend/teashop/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { OrdersListComponent } from './orders-list/orders-list.component';
+import { OrderComponent } from './order/order.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'orders-list', component: OrdersListComponent },
+  { path: 'product-details', component: ProductDetailsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
